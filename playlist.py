956e561e2b383edbfbd79616aaa353b9477919bf
Aaron Cuthbertson <3aaron3@gmail.com>
2020-10-04 22:04:45 +0100

    Start adding YouTube Music functionality (YT -> Spotify)

--- playlist.py
+++ playlist.py
@@ -1,6 +1,7 @@
 import gmusicapi
 import spotipy
 import spotipy.util as util
+from ytmusicapi import YTMusic
 import requests
 import re
 import os
@@ -21,6 +22,9 @@ ANDROID_MAC_ADDRESS = "XXXX"
 #Authenticates Spotify and Google Play Music
 token = util.prompt_for_user_token(USERNAME, SCOPE, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
 api = gmusicapi.Mobileclient()
+headers = YTMusic.setup(filepath="headers_auth.json", headers_raw="""HEADERS""")
+
+ytmusic = YTMusic(headers)
 
 #Checks for existing Google Play credentials, using them if found or creating them otherwise
 if not(os.path.exists("creds") and os.path.isfile("creds")):
@@ -196,6 +200,23 @@ def findGooglePlaylist(playlistName):
             break
     return targetPlaylist
 
+def findYtPlaylist(playlistName):
+    targetPlaylist = None
+
+    targetId = None
+    allPlaylists = ytmusic.get_library_playlists(200)
+    for playlist in allPlaylists:
+        #Find (first) playlist with matching name
+        if playlist["title"] == playlistName:
+            targetId = playlist["playlistId"]
+            break
+
+    if targetId:
+        playlist = ytmusic.get_playlist(targetId, 5000)
+        return playlist
+
+    return targetPlaylist
+
 #Request input for (Spotify) playlist URL and validate it
 def acceptPlaylistURL():
     while True: #loop until valid (at which point simply return)
@@ -248,6 +269,29 @@ def getInfoFromGooglePlaylist(targetPlaylist):
 
     return tracksInfo
 
+def getInfoFromYtPlaylist(targetPlaylist):
+    trackIDs = {}
+    count = 0
+    #Extract IDs of each track in the playlist
+    for track in targetPlaylist["tracks"]:
+        trackIDs[track["videoId"]] = count
+        count += 1
+
+    tracksInfo = [None] * count
+    library = ytmusic.get_library_upload_songs(100000)
+    #Looks for matching track in Google Play library
+    for track in library:
+        if track["videoId"] in trackIDs.keys():
+            #Add album, artist and track names to 2D array
+            info = []
+            info.append(track["album"]["name"])
+            info.append(track["artist"][0]["name"])
+            info.append(track["title"])
+            print(info)
+            tracksInfo[trackIDs[track["videoId"]]] = info
+
+    return tracksInfo
+
 #Adds a track (array with album, artist and track name) to a Google Play Music playlist with specified ID
 def addSongsToGooglePlaylistFromInfo(playlist_id, track):
     #track should be array with 3 elements
@@ -267,6 +311,66 @@ def addSongsToGooglePlaylistFromInfo(playlist_id, track):
             api.add_songs_to_playlist(playlist_id, track_ids) #adds track to playlist
             return True
     return False #could not find track in library
+
+def ytToSpotify():
+    print("Please enter the name of the YouTube Music playlist you would like to convert.")
+    print("Note that if multiple playlists exist with the same name, the first one will be selected; rename the playlist to be unique to avoid this.")
+    playlistName = input("| ") #Gets playlist name
+    targetPlaylist = findYtPlaylist(playlistName) #Finds playlist matching inputted name
+
+    if targetPlaylist == None:
+        print("No matching playlist found.")
+    else:
+        print("Matching playlist found.")
+
+        #Get playlist description, if there is one
+        try:
+            playlistDescription = targetPlaylist["description"]
+        except:
+            playlistDescription = ""
+
+        #Gets album, artist and track names for the contents of the playlist
+        tracksInfo = getInfoFromYtPlaylist(targetPlaylist)
+
+        print("Would you like the Spotify playlist to be public? (y/n)")
+        choice = input("| ")
+        public = False
+        if choice.lower() == "y":
+            public = True
+
+        #Create new Spotify playlist
+        spotifyPlaylist = spotify.user_playlist_create(USERNAME, playlistName, public=public, description=playlistDescription)
+
+        spotifyIDs = []
+        failures = []
+
+        #For every track from Google to be added
+        print(tracksInfo)
+        for track in tracksInfo:
+            #Check if track exists on Spotify, and get track object if so
+            result = findTrackOnSpotify(track[0], track[1], track[2])
+            if not result: #No matching track found
+                failure = [track[0], track[1], track[2]]
+                failures.append(failure)
+            else:
+                print(track[0] + " - " + track[1] + " - " + track[2])
+                spotifyIDs.append(result[0]["id"]) #Add ID of first matching result
+
+
+        if failures: #output any tracks that could not be found
+            print("Certain tracks could not be found on Spotify (these tracks may be unavailable, or have metadata that does not match Google's):")
+            missing_tracks = ["Missing from original playlist:"]
+            for failure in failures:
+                track = failure[0] + " - " + failure[1] + " - " + failure[2]
+                print(track)
+                missing_tracks.append(track)
+
+            new_description = playlistDescription + " | ".join(missing_tracks)
+            spotify.user_playlist_change_details(USERNAME, spotifyPlaylist["id"], description=new_description[:300]) # 300 character limit
+
+        #Add tracks to Spotify playlist
+        spotify.user_playlist_add_tracks(USERNAME, spotifyPlaylist["id"], spotifyIDs)
+        print("Playlist converted: https://open.spotify.com/playlist/" + str(spotifyPlaylist["id"]))
 
 #Converts a Spotify playlist to a Google Play Music playlist
 def spotifyToGoogle():
@@ -379,11 +483,17 @@ if token:
         print("What would you like to do?")
         print("(1) Convert Spotify playlist to Google Play Music")
         print("(2) Convert Google Play Music playlist to Spotify")
-        print("(3) Exit")
+        print("(3) Convert Spotify playlist to YouTube Music")
+        print("(4) Convert YouTube Music playlist to Spotify")
+        print("(5) Exit")
         choice = input("| ")
 
-        if choice == "3": #Exit
+        if choice == "5": #Exit
             con = False
+        elif choice == "4":
+            ytToSpotify()
+        elif choice == "3":
+            spotifyToYt()
         elif choice == "2":
             googleToSpotify()
         elif choice == "1":
